283d720ddd1773d68a783bcdf7e862f895f8fc26
Calvin Metcalf <calvin.metcalf@state.ma.us>
2014-08-22 20:37:49 -0400

    working, with tests

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@ var level = require('levelup');
 var tree = require('../lib');
 var sublevel = require('level-sublevel');
 var schools = require('./schools.json');
-
+var towns = require('./towns.json');
 test('load', function (t) {
   t.plan(4);
-  var db = tree(sublevel(level('./test_db',  { db: require('memdown') })));
+  var db = tree(sublevel(level('./test_db',  {valueEncoding:'json', db: require('memdown') })));
   db.put('1', schools.features[0], function (err) {
     t.error(err);
     db.del('1', function (err) {
@@ -27,4 +27,65 @@ test('load', function (t) {
       });
     });
   });
+});
+test('big query', function (t) {
+  t.plan(3);
+  var db = tree(sublevel(level('./test_db',  {valueEncoding:'json', db: require('memdown') })));
+   db.batch(schools.features.map(function (item) {
+    return {
+      key: item.id,
+      value: item
+    };
+   }), function (err) {
+    t.error(err);
+    db.once('uptodate', function () {
+      db.treeQuery([-100,0,0,80], function (err, resp) {
+        t.error(err);
+        t.equals(resp.length, 330, 'got then all');
+      });
+    });
+   });
+});
+test('actual query', function (t) {
+  t.plan(3);
+  var db = tree(sublevel(level('./test_db',  {valueEncoding:'json', db: require('memdown') })));
+   db.batch(schools.features.map(function (item) {
+    return {
+      key: item.id,
+      value: item
+    };
+   }), function (err) {
+    t.error(err);
+    db.once('uptodate', function () {
+      db.treeQuery([ -73.050957972443328, 41.813217236760636, -71.941805307670336,  42.95940187161159], function (err, resp) {
+        t.error(err);
+        t.equals(resp.length, 82, 'got right number');
+      });
+    });
+   });
+});
+
+test('point query', function (t) {
+  t.plan(4);
+  var db = tree(sublevel(level('./test_db',  {valueEncoding:'json', db: require('memdown') })));
+   db.batch(towns.features.map(function (item) {
+    return {
+      key: item.properties.TOWN,
+      value: item,
+      valueEncoding: 'json'
+    };
+   }), function (err) {
+    t.error(err);
+    db.once('uptodate', function () {
+      db.treeQuery([ -70.98495,42.24867,-70.98495,42.24867], function (err, resp) {
+        t.error(err);
+        t.equals(resp.length, 2, 'we get 2')
+        var nr = resp.map(function (i) {
+          return i.properties.TOWN;
+        });
+        nr.sort();
+        t.deepEqual(nr, ['BOSTON', 'QUINCY'], 'boston and quincy');
+      });
+    });
+   });
 });
